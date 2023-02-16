b81a4522989c766846ca0b89445a08002f207d86
Arafat817 <gr1arafat@gmail.com>
2023-02-16 18:46:30 +0600

    first commit

---
+++ lower-to-upper-case.js
@@ -0,0 +1,11 @@
+const userName = 'gulamRabbiArafat';
+const userInput = 'gulamRabbiArafat';
+
+console.log(userName.toLowerCase());
+console.log(userInput.toUpperCase());
+if(userName.toLowerCase()== userInput.toLowerCase()){
+    console.log('valid user')
+}
+else{
+    console.log('invalid user');
+}
